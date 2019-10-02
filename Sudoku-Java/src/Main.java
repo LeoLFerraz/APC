7398229701b454e750117c6cd8898ba6e7dc9afa
Cass <rita.soares@uniriotec.br>
2019-10-01 21:15:53 -0300

    Modificação de SudokuFileLoader para aceitar cabeçalho nos csvs

--- Sudoku-Java/src/Main.java
+++ Sudoku-Java/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		SudokuFileLoader loader = 
-				new SudokuFileLoader("..\\Sudoku-Java\\files\\sudoku.csv");
+				new SudokuFileLoader("..\\Sudoku-Java\\files\\sudoku.csv", true);
 
 	    // Exemplo: problema e solucao da linha 3 arquivo .csv:
 	    // loader.problems.get(3);
